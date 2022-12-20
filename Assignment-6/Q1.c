f322c2b70ac5f377d0839af830db1c7f3b4babc5
Anand Kumar <109381407+ashish-kr-anand@users.noreply.github.com>
2022-12-21 00:34:06 +0530

    Add files via upload

--- Assignment-6/Q1.c
+++ Assignment-6/Q1.c
@@ -1,1 +1,27 @@
-
+<<<<<<< HEAD
+// Write a program to print MySirG N times on the screen
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter n :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+     printf("MySirG\n");
+     return 0;
+}
+=======
+// Write a program to calculate sum of first N natural number
+#include<stdio.h>
+int main()
+{
+    int i,sum=0,n;
+    printf("Enter n num: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        sum=sum+i;
+    printf("Sum=%d ",sum);
+    return 0;
+     
+}
+>>>>>>> 106570c (upload)
